bf9b373482faf830f9cb1bbd43e80331844cb127
Michael Hernandez <mghernandezcastillo@gmail.com>
2021-10-27 21:49:27 -0500

    Deploy heroku

--- technodevicesProject/settings.py
+++ technodevicesProject/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0p*&zf-4=k*mj@+0+ff%&o+jfxg%jo3m=nyqvw08l(=vr0#(+8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 CORS_ALLOW_ALL_ORIGINS = True 
